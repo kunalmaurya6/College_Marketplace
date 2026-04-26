1120f5e2748c21ab41a3834d1bc87fe91e68831e
kunalmaurya6 <109097899+kunalmaurya6@users.noreply.github.com>
2026-04-26 09:44:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "https://apana-market.vercel.app/",
+    origin: "https://apana-market.vercel.app",
     credentials: true
 }));
 
