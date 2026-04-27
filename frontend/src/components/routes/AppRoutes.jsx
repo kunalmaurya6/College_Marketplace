1da321d259ba203cddb8e685e7fb64839a852c4c
kunalmaurya6 <mauryaankit86810@gmail.com>
2026-04-27 15:05:13 +0530

    page not found

--- frontend/src/components/routes/AppRoutes.jsx
+++ frontend/src/components/routes/AppRoutes.jsx
@@ -4,17 +4,24 @@ import Seller from '../seller/Seller'
 import { AddProduct, Listings, Messages,Profile,ProductView, Admin } from '../seller/pages/pages'
 import User from '../user/User'
 import Home from '../user/home/Home'
+import NotFound from '../utils/NotFound'
 
 const AppRoutes = () => {
+
     return (
         <Routes>
-            <Route path="/admin" element={<Admin/>}></Route>
+
+            <Route path="/admin" element={<Admin/>}/>
+
             <Route path="/product" element={<ProductView />} />
+
             <Route path='/' element={<User />} >
                 <Route index element={<Home/>}/>
                 <Route path='profile' element={<Profile />} />
             </Route>
+
             {/* <Route path="/chat" element={<Chat />} /> */}
+
             <Route path="/seller" element={<Seller />} >
                 <Route index element={<Listings />} />
                 <Route path="product" element={<AddProduct />} />
@@ -22,7 +29,11 @@ const AppRoutes = () => {
                 <Route path="profile" element={<Profile />} />
                 {/* <Route path="logout" element={<Logout />} /> */}
             </Route>
+
+            <Route path="*" element={<NotFound />} />
+
         </Routes>
+        
     )
 }
 
